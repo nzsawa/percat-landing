03216a86e80b72d698cde53b99be4b02128d754a
Payam Chamani <pchamani@nizek.com>
2025-08-29 02:36:28 +0330

    posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { PostHogProvider } from "posthog-js/react";
 import "./index.css";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true,
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <App />
+    </PostHogProvider>
   </React.StrictMode>
 );
-
